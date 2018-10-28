0a032c5b004e9eaaa584799a46f395c4f960830b
Dhruvika Sahni <dhruvika@mit.edu>
2018-10-28 16:00:01 -0400

    loading all messages (basic functionality)

--- mobile/src/main/java/com/example/dhruvikasahni/lvtexting/MainActivity.java
+++ mobile/src/main/java/com/example/dhruvikasahni/lvtexting/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.dhruvikasahni.lvtexting;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lv = findViewById(R.id.SmsList);
+
+        if(fetchInbox() != null) {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fetchInbox());
+            lv.setAdapter(adapter);
+        }
+    }
+    public ArrayList<String> fetchInbox(){
+        ArrayList<String> sms = new ArrayList<>();
+        Uri uri = Uri.parse("content://sms/inbox");
+        Cursor cursor = getContentResolver().query(uri, new String[]{"id", "address", "date", "body"}, null, null, null);
+
+        cursor.moveToFirst();
+        while(cursor.moveToNext()){
+            String address = cursor.getString(1);
+            String body = cursor.getString(3);
+            sms.add("Name: " + address + "\n Message: "+body);
+        }
+        return sms;
     }
 }
