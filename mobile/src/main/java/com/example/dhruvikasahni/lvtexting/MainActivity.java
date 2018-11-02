3c0302af9b540ca5b7ebb759bfa59d940509e2c5
Dhruvika Sahni <dhruvika@mit.edu>
2018-11-02 12:56:16 -0400

    loading all messages (basic functionality)

--- mobile/src/main/java/com/example/dhruvikasahni/lvtexting/MainActivity.java
+++ mobile/src/main/java/com/example/dhruvikasahni/lvtexting/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.dhruvikasahni.lvtexting;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,24 +22,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lv = findViewById(R.id.SmsList);
-
-        if(fetchInbox() != null) {
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fetchInbox());
-            lv.setAdapter(adapter);
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_SMS)){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[] {Manifest.permission.READ_SMS}, 1);
+            }
+            else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_SMS}, 1);
+            }
+        }
+        else {
+            // do nothing
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case 1: {
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED) {
+                        // permission granted
+                        ListView lv = findViewById(R.id.SmsList);
+
+                        if (fetchInbox().isEmpty()) {
+                            return;
+                        }
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                                android.R.layout.simple_list_item_1, fetchInbox());
+                        lv.setAdapter(adapter);
+                    }
+                } else {
+                    // no permission granted
+                }
+            }
+        }
+    }
+
     public ArrayList<String> fetchInbox(){
         ArrayList<String> sms = new ArrayList<>();
         Uri uri = Uri.parse("content://sms/inbox");
-        Cursor cursor = getContentResolver().query(uri, new String[]{"id", "address", "date", "body"}, null, null, null);
+        Cursor cursor = getContentResolver().query(uri, new String[]{"_id", "address", "date", "body"},
+                null, null, null);
 
-        cursor.moveToFirst();
-        while(cursor.moveToNext()){
-            String address = cursor.getString(1);
-            String body = cursor.getString(3);
-            sms.add("Name: " + address + "\n Message: "+body);
+        try {
+            cursor.moveToFirst();
+            while(cursor.moveToNext()) {
+                String address = cursor.getString(1);
+                String body = cursor.getString(3);
+                sms.add("Name: " + address + "\n Message: " + body);
+            }
+            cursor.close();
         }
+        catch(NullPointerException e){
+            // do nothing
+        }
+
         return sms;
     }
 }
