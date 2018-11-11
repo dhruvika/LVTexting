da0d7f52c9063b8b259674da450d601316e74d57
Abhiti Vaish <abhiti@mit.edu>
2018-11-10 18:59:55 -0800

    updaed theme to work as expected with color inversion

--- mobile/src/main/java/com/example/dhruvikasahni/lvtexting/MainActivity.java
+++ mobile/src/main/java/com/example/dhruvikasahni/lvtexting/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dhruvikasahni.lvtexting;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -10,7 +11,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.textmessageslibrary.TextMessageFetcher;
@@ -34,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this,
                     new String[]{Manifest.permission.READ_SMS}, 1);
         }
+        Button convo = (Button) findViewById(R.id.convo); //FOR CONVERSATION DEBUGGING (Abhiti will remove)
+        convo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), Conversation.class));
+            }
+        });
     }
 
     @Override
