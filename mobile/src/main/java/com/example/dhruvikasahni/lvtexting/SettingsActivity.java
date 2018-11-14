a743f8c3039720eb14dd4d79c3d86e7810339e3e
Femi Oladipupo <Femi@DESKTOP-D8TQN6P.mit.edu>
2018-11-14 02:44:03 -0500

    Add SettingsManager class for applying preferences to views

--- mobile/src/main/java/com/example/dhruvikasahni/lvtexting/SettingsActivity.java
+++ mobile/src/main/java/com/example/dhruvikasahni/lvtexting/SettingsActivity.java
@@ -19,19 +19,20 @@ import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private SharedPreferences sharedPref;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        Context context = this;
-        sharedPref = context.getSharedPreferences("prefs", Context.MODE_PRIVATE);
         onFontChange();
     }
 
+    public void onFontChange() {
+        SettingsManager.onFontChange(this, (ViewGroup) findViewById(R.id.SETTINGS_CONTAINER));
+    }
     public void onChangeFontSize(View v) {
+
+        SharedPreferences sharedPref = SettingsManager.getSharedPreferences(this);
 
         // Get preference
         int buttonID = v.getId();
@@ -49,7 +50,7 @@ public class SettingsActivity extends AppCompatActivity {
         int MAX_FONT_SIZE = 50;
         int MIN_FONT_SIZE = 14;
 
-        int currentSize = sharedPref.getInt(FONT_SIZE,20);
+        int currentSize = sharedPref.getInt(FONT_SIZE, 20);
 
         // Change value
         int newSize = currentSize + change;
@@ -64,38 +65,5 @@ public class SettingsActivity extends AppCompatActivity {
         sharedPref.edit().putInt(FONT_SIZE, newSize).apply();
         onFontChange();
     }
-
-    public void onFontChange() {
-        // Set style from preference
-        int currentSize = sharedPref.getInt("FONT_SIZE",20);
-
-        ViewGroup settingsContainer = findViewById( R.id.SETTINGS_CONTAINER );
-        List<TextView> textViews = getViewsFromGroup(settingsContainer);
-
-        for( int i = 0; i < textViews.size(); i++ ) {
-            textViews.get(i).setTextSize(TypedValue.COMPLEX_UNIT_SP, currentSize);
-        }
-    }
-
-    public List<TextView> getViewsFromGroup(ViewGroup viewGroup) {
-        /*
-        Recursive method that finds all textViews in given viewGroup and returns them as a list
-         */
-
-        List<TextView> viewList = new ArrayList<>();
-
-        for( int i = 0; i < viewGroup.getChildCount(); i++ ) {
-            if (viewGroup.getChildAt(i) instanceof TextView) {
-                TextView textView = (TextView) viewGroup.getChildAt(i);
-                viewList.add(textView);
-            }
-
-            if (viewGroup.getChildAt(i) instanceof ViewGroup) {
-                ViewGroup childViewGroup = (ViewGroup) viewGroup.getChildAt(i);
-                viewList.addAll(getViewsFromGroup(childViewGroup));
-            }
-        }
-
-        return viewList;
-    }
 }
+
