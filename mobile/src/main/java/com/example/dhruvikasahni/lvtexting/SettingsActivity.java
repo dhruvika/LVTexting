73e0aecf231f76336ada856222d19e1aca3685a8
femi96 <imef96@gmail.com>
2018-12-08 16:18:36 -0500

    Update SettingsManager to manage preferences and styles

--- mobile/src/main/java/com/example/dhruvikasahni/lvtexting/SettingsActivity.java
+++ mobile/src/main/java/com/example/dhruvikasahni/lvtexting/SettingsActivity.java
@@ -30,39 +30,24 @@ public class SettingsActivity extends AppCompatActivity {
         SettingsManager.applySettingsToTheme(this);
     }
 
-    public void onChangeFontSize(View v) {
-
-        SharedPreferences sharedPref = SettingsManager.getSharedPreferences(this);
+    public void onChangePref(View v) {
 
         // Get preference
         int buttonID = v.getId();
-        int change = 0;
         switch (buttonID) {
-            case R.id.FONT_SIZE_I:
-                change = 1;
+            case R.id.font_size_i:
+                SettingsManager.changeFontSize(this, 1);
                 break;
-            case R.id.FONT_SIZE_D:
-                change = -1;
+            case R.id.font_size_d:
+                SettingsManager.changeFontSize(this, -1);
+                break;
+            case R.id.line_space_i:
+                SettingsManager.changeFontSize(this, 1);
+                break;
+            case R.id.line_space_d:
+                SettingsManager.changeFontSize(this, -1);
                 break;
         }
-
-        String FONT_SIZE = "FONT_SIZE";
-        int MAX_FONT_SIZE = 50;
-        int MIN_FONT_SIZE = 14;
-
-        int currentSize = sharedPref.getInt(FONT_SIZE, 20);
-
-        // Change value
-        int newSize = currentSize + change;
-        if (newSize > MAX_FONT_SIZE) {
-            newSize = MAX_FONT_SIZE;
-        }
-        if (newSize < MIN_FONT_SIZE) {
-            newSize = MIN_FONT_SIZE;
-        }
-
-        // Set preference (Try using commit instead of apply)
-        sharedPref.edit().putInt(FONT_SIZE, newSize).apply();
         applySettingsToTheme();
     }
 }
--- mobile/src/main/java/com/example/dhruvikasahni/lvtexting/SettingsManager.java
+++ mobile/src/main/java/com/example/dhruvikasahni/lvtexting/SettingsManager.java
@@ -2,6 +2,7 @@ package com.example.dhruvikasahni.lvtexting;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -11,7 +12,7 @@ import java.util.List;
 
 public class SettingsManager {
 
-    public static SharedPreferences getSharedPreferences(Context context) {
+    private static SharedPreferences getSharedPreferences(Context context) {
         /*
         Get shared preferences for this app
          */
@@ -20,15 +21,66 @@ public class SettingsManager {
         return sharedPref;
     }
 
+    private static final String FONT_SIZE = "FONT_SIZE";
+
     public static void applySettingsToTheme(Context context) {
         /*
         Apply settings changes to the context's theme
          */
-
-        /*
         SharedPreferences sharedPref = getSharedPreferences(context);
-        int currentSize = sharedPref.getInt("FONT_SIZE",20);
-         */
-        context.getTheme().applyStyle(R.style.FontStyle_Medium, true);
+
+        // Apply font size
+        int currentFontSize = sharedPref.getInt(FONT_SIZE,5);
+        switch (currentFontSize) {
+            case 1:
+                context.getTheme().applyStyle(R.style.FontSize_S1, true);
+                break;
+            case 2:
+                context.getTheme().applyStyle(R.style.FontSize_S2, true);
+                break;
+            case 3:
+                context.getTheme().applyStyle(R.style.FontSize_S3, true);
+                break;
+            case 4:
+                context.getTheme().applyStyle(R.style.FontSize_S4, true);
+                break;
+            case 5:
+                context.getTheme().applyStyle(R.style.FontSize_S5, true);
+                break;
+            case 6:
+                context.getTheme().applyStyle(R.style.FontSize_S6, true);
+                break;
+            case 7:
+                context.getTheme().applyStyle(R.style.FontSize_S7, true);
+                break;
+            case 8:
+                context.getTheme().applyStyle(R.style.FontSize_S8, true);
+                break;
+            case 9:
+                context.getTheme().applyStyle(R.style.FontSize_S9, true);
+                break;
+            default:
+                context.getTheme().applyStyle(R.style.FontSize_S5, true);
+                break;
+        }
+    }
+
+    public static void changeFontSize(Context context, int delta) {
+        /*
+        Change font size by delta
+        */
+        SharedPreferences sharedPref = getSharedPreferences(context);
+        int MAX_FONT_SIZE = 9;
+        int MIN_FONT_SIZE = 1;
+
+        // Calculate new size
+        int currentSize = sharedPref.getInt(FONT_SIZE, 5);
+        int newSize = currentSize + delta;
+        if (newSize > MAX_FONT_SIZE) { newSize = MAX_FONT_SIZE; }
+        if (newSize < MIN_FONT_SIZE) { newSize = MIN_FONT_SIZE; }
+
+        // Set preference (Try using commit instead of apply)
+        sharedPref.edit().putInt(FONT_SIZE, newSize).apply();
+        // Log.d("MyDebug", Integer.toString(newSize));
     }
 }
